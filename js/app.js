486d753ccbe7aaaab77bb369fb6eb4b380809bf5
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2023-11-14 19:26:50 -0500

    added animation to member list on home page so it will scroll

--- js/app.js
+++ js/app.js
@@ -179,6 +179,9 @@ function toggleTutorial() {
     var tutorial = document.querySelector('.tutorial-container');
     tutorial.style.display = (tutorial.style.display === 'none' || tutorial.style.display === '') ? 'block' : 'none';
   }
+
+ 
+
 /* TEST METHODS/FUNCTIONS */
 // Add a click event listener to the canvas
 //canvas.addEventListener("click", function (e) {
